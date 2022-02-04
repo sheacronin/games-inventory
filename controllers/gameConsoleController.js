1c3d35787882424db23181d2df2590a6e2526258
Shea Cronin <shealcronin@gmail.com>
2022-02-04 16:40:30 -0500

    Add initial console detail view

--- controllers/gameConsoleController.js
+++ controllers/gameConsoleController.js
@@ -1,4 +1,6 @@
 const GameConsole = require('../models/gameConsole');
+const Game = require('../models/game');
+const async = require('async');
 
 // Display list of all consoles
 exports.gameConsoleList = (req, res) => {
@@ -15,7 +17,30 @@ exports.gameConsoleList = (req, res) => {
 
 // Display detail page of console with its games
 exports.gameConsoleDetail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Game console detail: ' + req.params.id);
+    async.parallel(
+        {
+            gameConsole: (callback) => {
+                GameConsole.findById(req.params.id).exec(callback);
+            },
+            games: (callback) => {
+                Game.find({ gameConsoles: req.params.id }).exec(callback);
+            },
+        },
+        (err, results) => {
+            if (err) return next(err);
+            if (results.gameConsole == null) {
+                var err = new Error('Console not found');
+                err.status = 404;
+                return next(err);
+            }
+            const { gameConsole, games } = results;
+            res.render('console_detail', {
+                title: results.gameConsole.name,
+                gameConsole,
+                games,
+            });
+        }
+    );
 };
 
 // Display console create page on GET
