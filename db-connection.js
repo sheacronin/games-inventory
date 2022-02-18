3d45f342557dd3ce1549c46051dd191dcf4a3995
Shea Cronin <shealcronin@gmail.com>
2022-02-18 17:56:40 -0500

    Move MongoDB url to env variable

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,6 @@
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var devDbUrl =
-    'mongodb+srv://shea:nugk58qg3zEEDxSF@cluster0.tbvdy.mongodb.net/games_inventory?retryWrites=true&w=majority';
+var devDbUrl = process.env.MONGODB_DEV;
 var mongoDB = process.env.MONGODB_URI || devDbUrl;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
